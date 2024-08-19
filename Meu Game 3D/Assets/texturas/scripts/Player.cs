6ba546fbd888c0ee5b8b9cf7672d40b512f04faf
HenriqueDev15 <oliveira.vasconcelos@escolar.ifrn.edu.br>
2024-08-19 11:53:39 -0300

    Script do Player

---
+++ Meu Game 3D/Assets/texturas/scripts/Player.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+using Vector3 = UnityEngine.Vector3;
+public class Player : MonoBehaviour
+{
+    public int velocidade = 10;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("start");
+        TryGetComponent(out rb);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         Debug.Log("update");
+         float x = Input.GetAxis("Horizontal");
+         float y = Input.GetAxis("Vertical");
+
+         UnityEngine.Vector3 direcao = new UnityEngine.Vector3(x,0,y);
+         rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
+
+
+    }
+}
